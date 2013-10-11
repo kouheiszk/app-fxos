b4e458571c7e092bc62ec79cbff43aebf349bd5b
Kouhei Suzuki <kouhei.szk@gmail.com>
2013-10-11 19:13:11 +0900

    scroll able

--- js/app.js
+++ js/app.js
@@ -7,17 +7,17 @@ Namespace('app')
     var getFriends = function() {
         ns.call("https://api.mixi-platform.com/2/people/@me/@friends")
         .bind(ns.promise(function(next, value) {
-          var $friendsArea = $($(".myFriends")[0]);
-          for(var i = 0; i < value.entry.length; i++){
-            var friendData = value.entry[i];
-            console.log(friendData.displayName);
-            var $friend = $("<li class='friend'></li>");
-            var $friendImage = $("<aside class='pack-end'><img src='" + friendData.thumbnailUrl + "' /></aside>");
-            var $friendName = $("<p class='friendName'>" + friendData.displayName + "</p>");
-            $friend.append($friendImage);
-            $friend.append($friendName);
-            $friendsArea.append($friend);
-          }
+            var $friendsArea = $($(".myFriends")[0]);
+            for(var i = 0; i < value.entry.length; i++){
+                var friendData = value.entry[i];
+                console.log(friendData.displayName);
+                var $friend = $('<li class="friend"></li>');
+                var $friendImage = $('<aside class="pack-end"><img alt="placeholder" src="' + friendData.thumbnailUrl + '"></aside>');
+                var $friendName = $('<a href="#"><p class="friendName">' + friendData.displayName + '</p></a>');
+                $friend.append($friendImage);
+                $friend.append($friendName);
+                $friendsArea.append($friend);
+            }
         })).run();
     };
 
