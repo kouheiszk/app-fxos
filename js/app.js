8898090d002366b2ef369acc61a26507d0674f43
Yuya Hasegawa <yuya.hasegawa@gmail.com>
2013-10-11 13:44:54 +0900

    showing friends list

--- js/app.js
+++ js/app.js
@@ -7,7 +7,17 @@ Namespace('app')
     var getFriends = function() {
         ns.call("https://api.mixi-platform.com/2/people/@me/@friends")
         .bind(ns.promise(function(next, value) {
-            console.log(value.entry[0].displayName);
+          var $friendsArea = $($(".myFriends")[0]);
+          for(var i = 0; i < value.entry.length; i++){
+            var friendData = value.entry[i];
+            console.log(friendData.displayName);
+            var $friend = $("<div class='friend'></div>");
+            var $friendImage = $("<img src='" + friendData.thumbnailUrl + "' />");
+            var $friendName = $("<div class='friendName'>" + friendData.displayName + "</div>");
+            $friend.append($friendImage);
+            $friend.append($friendName);
+            $friendsArea.append($friend);
+          }
         })).run();
     };
 
